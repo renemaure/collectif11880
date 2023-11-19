b6fcd81bcfbb59ad1600d2439fd6b5dd7df8de70
renemaure <pascalrollin290@gmail.com>
2023-11-19 13:39:52 +0100

    modification du texte dans la page association

--- pages/association.php
+++ pages/association.php
@@ -2,6 +2,32 @@
 			<div class="boite12">
 				<h4>Présentation de l'association</h4>
 				<p>
+				L’association fut créé en 1996, dans l’Yonne sous le nom du club CMIT. Elle regroupe des bidouilleurs, des passionnés, mais aussi des novices, qui veulent sauvegarder, réparer de vieux ordinateurs et faire de la programmation ludique ou de la robotique singulière.
+				</p>
+				<br>
+				<p>
+					Depuis 2007, notre association prend le nom de Collectif 11880, club CMIT, en intégrant en son sein le collectif 11880, né d’artistes belges et français et des bidouilleurs du CMIT, qui se sont regroupés pour exposer leurs ouvres électronique en Belgique à la biennale de Gant en 2004
+				</p>
+				<br>
+				<p>
+					Le coté collectif 11880, c’est la rencontre entre des techniciens et des artistes mettant en commun leurs compétences, avec comme grande ambition, de faire entrer le numérique dans l’art et l’art dans le numérique.
+				</p>
+			</div>
+			<!-- <div class="boite12">
+				<h4>Le collectif 11880</h4>
+				<p>
+					Texte à écrire par une bonne volonté d'un membre de l'assos, donc le président??? A voir...
+				</p>
+			</div> -->
+			<div class="boite12">
+				<h4>Le club CMIT</h4>
+				<p>
+					Le collectif c'est aussi et toujours le Club CMIT, le club des collectionneurs d’ordinosaures qui gère entre autres la collection du président pour les mettre à disposition de ses membres et du public.
+				</p>
+			</div>
+			<div class="boite12">
+			<h4>Historique de l'association</h4>
+				<p>
 					Notre association a été créée le 22 janvier 1996 à Saint Martin du Tertre dans l’Yonne sous le nom de Conservatoire du matériel industriel et technique, ou CMIT. Après huit ans de fonctionnement dans l’Yonne et son expulsion du foyer des jeunes travailleurs de Sens notre association déménage à Paris dans le 13ème et en profite pour modifier son nom et devient le collectif 11880 club CMIT, pour plus de 14 ans de fonctionnement et pas mal d’aventures.
 				</p>
 				<p>
@@ -10,18 +36,7 @@
 					Premier chantier pour cette année 2021 consiste à refaire entièrement un notre site internet qui en est déjà à sa à sa sixième mouture, mais aussi qu’un nouveau projet de robotique avec la création d’un assistant trop personnel. Le collectif continu aussi les projets Matricule et erreur Système, ainsi qu’un nouveau projet de diffusion d’art libre nommé ProjetX.			
 				</p>
 			</div>
-			<div class="boite12">
-				<h4>Le collectif 11880</h4>
-				<p>
-					Texte à écrire par une bonne volonté d'un membre de l'assos, donc le président??? A voir...
-				</p>
-			</div>
-			<div class="boite12">
-				<h4>Le club CMIT</h4>
-				<p>
-					Le collectif c'est aussi et toujours le Club CMIT, le club des collectionneurs d’ordinosaures qui gère entre autres la collection du président pour les mettre à disposition de ses membres et du public.
-				</p>
-			</div>
+	
 			<!-- <div class="boite12">
 				<h4 class="titre-asso"><img src="systeme/images/logo_projetX_pt.png"></h4>
 				<p>
